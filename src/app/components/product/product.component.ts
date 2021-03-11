5645447150314795dab7fc06a4e734dbf35b26e7
Esra S <esrasancak@hotmail.com>
2021-03-11 16:26:38 +0300

    1.Ders

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -7,20 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  product:any={
-  productId:1,
-  productName:'Bardak',
-  categoryId:1,
-  unitPrice:5
-}
-
-product1:any={productId:2,productName:'Camera',categoryId:1,unitPrice:5}
-product2:any={productId:3,productName:'Laptop',categoryId:1,unitPrice:5} 
-product3:any={productId:4,productName:'Mouse',categoryId:1,unitPrice:5}
-product4:any={productId:5,productName:'Keyboard',categoryId:1,unitPrice:5}
-product5:any={productId:6,productName:'Kardan Adam',categoryId:1,unitPrice:5}
-
-products =[this.product,this.product1,this.product2,this.product3,this.product4,this.product5];
+products:Product[] =[];
 
 
   constructor() { }
---
+++ src/app/models/product.ts
@@ -0,0 +1,8 @@
+export interface Product{
+   productId:number;
+   categoryId:number;
+   productName:string;
+   unitsInStock:number;
+   unitPrice:number;
+
+}
